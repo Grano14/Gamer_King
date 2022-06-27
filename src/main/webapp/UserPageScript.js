fc93ea5f30059ac578c6f20c72dd1a9dc14462aa
SalvatoreDL01 <104557779+SalvatoreDL01@users.noreply.github.com>
2022-06-27 11:27:53 +0200

    prova1

--- src/main/webapp/UserPageScript.js
+++ src/main/webapp/UserPageScript.js
@@ -1,5 +1,5 @@
 function showMenu(val){
-    var list = ["m", "m1", "m2", "m3"];
+    var list = ["m", "m1", "m2", "m3", "m4", "m5", "m6"];
     list.forEach(function (element) {
         if(element == val)
             document.getElementById(element).style.visibility = "visible";
