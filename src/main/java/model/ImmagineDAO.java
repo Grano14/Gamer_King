fdda26d2bc675425aab15d271ea3a230f5d71bd1
Grano14 <104316561+Grano14@users.noreply.github.com>
2022-07-15 19:07:07 +0200

    Aggiunta dao

---
+++ src/main/java/model/ImmagineDAO.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ImmagineDAO {
+
+    public static Immagine doRetriveById(String path){
+        try(Connection con = ConPool.getConnection()){
+            PreparedStatement ps = con.prepareStatement("select path, videogioco from Immagine where path=?");
+            ps.setString(1, path);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                Immagine i = new Immagine(rs.getString(1), rs.getString(2));
+                return i;
+            }
+            return null;
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void doSave(Immagine i){
+        try(Connection con = ConPool.getConnection()){
+            PreparedStatement ps = con.prepareStatement("insert into Immagine (path, videogioco) values (?,?)");
+            ps.setString(1, i.getPath());
+            ps.setString(2, i.getVideogioco());
+            ps.execute();
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void doRemoveById(String path){
+        try(Connection con = ConPool.getConnection()){
+            PreparedStatement ps = con.prepareStatement("delete from Immagine where path=?");
+            ps.setString(1, path);
+            ps.execute();
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/main/java/model/ProdottoDAO.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProdottoDAO {
+
+    public static Prodotto doRetriveById(String videogioco, String piattaforma){
+        try(Connection con = ConPool.getConnection()){
+            PreparedStatement ps = con.prepareStatement("select piattaforma, visibilita, datauscita, disponibilita, videogioco from Prodotto where videogioco=? and piattaforma=?");
+            ps.setString(1, videogioco);
+            ps.setString(2, piattaforma);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                Prodotto p = new Prodotto(rs.getString(1), rs.getString(3), rs.getString(5), rs.getBoolean(2), rs.getBoolean(4));
+                return p;
+            }
+            return null;
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void doSave(Prodotto p){
+        try(Connection con = ConPool.getConnection()){
+            PreparedStatement ps = con.prepareStatement("insert into Prodotto (piattaforma, visibilita, dataUscita, disponibilita, videogioco) values (?,?,?,?,?)");
+            ps.setString(1, p.getPiattaforma());
+            ps.setBoolean(2, p.isVisibilita());
+            ps.setString(3, p.getDataUscita());
+            ps.setBoolean(4, p.isDisponibilita());
+            ps.setString(5, p.getVideogioco());
+            ps.execute();
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void doRemoveById(String videogioco, String piattaforma){
+        try(Connection con = ConPool.getConnection()){
+            PreparedStatement ps = con.prepareStatement("delete from Prodotto where videogioco=? and piattaforma=?");
+            ps.setString(1, videogioco);
+            ps.setString(2, piattaforma);
+            ps.execute();
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+}
